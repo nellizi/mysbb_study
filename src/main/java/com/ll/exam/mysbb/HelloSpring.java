cfa78c5865a1e51b32dc0d189f743eadd5b2a882
nellizi <masterliz9095@gmail.com>
2022-08-19 18:32:21 +0900

    좋아요 기능 구현 테스트

--- src/main/java/com/ll/exam/mysbb/HelloSpring.java
+++ src/main/java/com/ll/exam/mysbb/HelloSpring.java
@@ -1,15 +1,17 @@
 package com.ll.exam.mysbb;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@AllArgsConstructor
 public class HelloSpring {
 
     @RequestMapping("/hello")
-    @ResponseBody
+
     public String helloSpring(){
-        return "Hello Spring!";
+        return "likebutton";
     }
 }
